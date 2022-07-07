3bb5d28363edb5de202b086226a95940d03716a8
psychokore <juju.faure@yahoo.fr>
2022-07-07 10:52:39 +0200

    correction requete api et redirection navigate

--- client/src/Layout/Auth/SignInForm.jsx
+++ client/src/Layout/Auth/SignInForm.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
-import postConnexion from "../../API/auth";
-
+//import postConnexion from "../../API/auth";
+import {useNavigate} from 'react-router-dom';
 
 const SignInForm = () =>{
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [hasError, setHasError] = useState (false)
+  const navigate = useNavigate()
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    postConnexion(email, password)
+    const url = 'http://localhost:3000/api/auth/login';
+    const data = {
+      email: email,
+      password: password
+    };
+
+    fetch(url,{
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+      }
+    })
       .then((res) => {
         if (res.status !== 200) {
           return setHasError(true);
+        } else {
+          navigate('../home', {replace: true});
         }
       })
       .catch ((err) => {
