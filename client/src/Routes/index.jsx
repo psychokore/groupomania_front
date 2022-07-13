f5025a24d255fe3c6ed69939f63a9b717f2e7667
psychokore <juju.faure@yahoo.fr>
2022-07-13 15:00:06 +0200

    renommage

--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from "../Pages/Login";
-import Profil from "../Pages/Profil";
-import Home from "../Pages/Home";
+import Login from "../pages/Login";
+import Profil from "../pages/Profil";
+import Home from "../pages/Home";
 // import { UidProvider } from "../utils/context";
 
 const index = () =>{
