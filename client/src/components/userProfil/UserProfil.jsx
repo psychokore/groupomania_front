df90655a53950915d1d93cf3c8a6da5eb0ce92c6
psychokore <juju.faure@yahoo.fr>
2022-08-11 18:25:11 +0200

    affichage des userdata

--- client/src/components/userProfil/UserProfil.jsx
+++ client/src/components/userProfil/UserProfil.jsx
@@ -5,29 +5,35 @@ import { getUserData } from '../../api/auth';
 
 const UserProfil = () => {
     const [userData, setUserData] = useState()
+    const [isLoading, setIsLoading] = useState(true)
     const token = useSelector((state) => state.user.token)
 
     useEffect(() => {
         const loadUserData = async () => {
             const user = await getUserData(token)
-            setUserData({firstname: user.data.firstname, lastname: user.data.lastname, email: user.data.email})    
+            setUserData({...user.data})   
+            setIsLoading(false) 
         }
         loadUserData()
     }, [])
+
+    
 
 
     return (
         <div className='profil-container'>
             <h1 className='data-title'> Mes informations</h1>
+            {isLoading ? <p>Chargement</p>: <>
             <div className='data-container'>
                 <p className='label'>Nom</p>
-                <p className='data'>Dupont</p>
+                <p className='data'>{userData.firstname}</p>
                 <p className='label'>Prénom</p>
-                <p className='data'>Michel</p>
+                <p className='data'>{userData.lastname}</p>
                 <p className='label'>Email</p>
-                <p className='data'>dupont.michel@gmail.com</p>
+                <p className='data'>{userData.email}</p>
             </div>
-            <button className='delete-profil'>Supprimer votre profil</button> 
+            <button className='delete-profil'>Supprimer votre profil</button>
+            </>}
         </div>
     );
 };
